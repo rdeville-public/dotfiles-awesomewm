a27d5b4889ac46ff00b65241c192dfee735c974a
Romain Deville <code@romaindeville.fr>
2024-07-28 23:52:02 +0200

    ⚡️(widgets/bat): Improve perf and reduce code qtty

--- widgets/bat.lua
+++ widgets/bat.lua
@@ -1,17 +1,13 @@
----@diagnostic disable undefined-global
 local awful = require("awful")
-local wibox = require("wibox")
-local gears = require("gears")
-local beautiful = require("beautiful")
+local beautiful = require("widgets.theme")
 local naughty = require("naughty")
+local wibox = require("wibox")
 
 local bat = {}
 
-local function factory(args)
-  local default_commands = {}
+local function factory(_)
   local power_info_dir = "/sys/class/power_supply/"
   local bat_info_dir = scandir(power_info_dir)
-  args = args or {}
 
   if bat_info_dir ~= nil and #bat_info_dir > 0 then
     local found = false
@@ -30,149 +26,57 @@ local function factory(args)
     return nil
   end
 
-  default_commands = {}
-  default_commands.status = "cat "
-    .. power_info_dir
-    .. bat_info_dir
-    .. "/status"
-  default_commands.capacity = "cat "
-    .. power_info_dir
-    .. bat_info_dir
-    .. "/capacity"
+  local options = {
+    timeout = beautiful.bat_timeout or 60,
+    cmd_status = beautiful.bat_cmd_status
+      or ("cat " .. power_info_dir .. bat_info_dir .. "/status"),
+    cmd_capacity = beautiful.bat_cmd_capacity
+      or ("cat " .. power_info_dir .. bat_info_dir .. "/capacity"),
+    clr_0 = beautiful.bat_clr_0
+      or beautiful.clr_0_inv
+      or beautiful.default_clr_0_inv,
+    clr_1 = beautiful.bat_clr_1
+      or beautiful.clr_1_inv
+      or beautiful.default_clr_1_inv,
+    clr_2 = beautiful.bat_clr_2
+      or beautiful.clr_2_inv
+      or beautiful.default_clr_2_inv,
+    clr_3 = beautiful.bat_clr_3
+      or beautiful.clr_3_inv
+      or beautiful.default_clr_3_inv,
+    icon_charging_0 = beautiful.bat_icon_charging_0 or "󰂎󱐥",
+    icon_charging_1 = beautiful.bat_icon_charging_1 or "󱊡󱐥",
+    icon_charging_2 = beautiful.bat_icon_charging_2 or "󱊢󱐥",
+    icon_charging_3 = beautiful.bat_icon_charging_3 or "󱊣󱐥",
+    icon_discharging_0 = beautiful.bat_icon_discharging_0 or "󰂎 ",
+    icon_discharging_1 = beautiful.bat_icon_discharging_1 or "󱊡 ",
+    icon_discharging_2 = beautiful.bat_icon_discharging_2 or "󱊢 ",
+    icon_discharging_3 = beautiful.bat_icon_discharging_3 or "󱊣 ",
+    alert_value = beautiful.bat_alert_value or 10,
+    alert_icon = beautiful.bat_alert_icon
+      or script_path() .. "icons/dialog-warning.svg",
+  }
 
-  args.font = args.font or beautiful.bat_font or beautiful.font
-  args.bg = args.bg or beautiful.bat_bg or "#000000"
-  args.fg = args.fg or beautiful.bat_fg or "#FFFFFF"
-  args.height = args.height or beautiful.bat_height or 24
-  args.icon = args.icon or beautiful.bat_icon or "BAT: "
-  args.timeout = args.timeout or beautiful.bat_timeout or 1
+  local update_widget = function(widget)
+    local value = tonumber(os.capture(options.cmd_capacity))
+    local clr_idx = math.floor(value / 25) == 4 and 3 or math.floor(value / 25)
+    local status = os.capture(options.cmd_status)
 
-  args.shape = args.shape or beautiful.bat_shape or gears.shape.rect
-
-  args.bar_shape = args.bar_shape or beautiful.bat_bar_shape
-  args.bar_width = args.bar_width or beautiful.bat_bar_width or 10
-  args.bar_fg = args.bar_fg or beautiful.bat_bar_fg or args.fg
-  args.bar_bg = args.bar_bg or beautiful.bat_bar_bg or args.bg
-
-  args.alert_value = args.alert_value or beautiful.bat_alert_value or 25
-
-  args.tier1_val = args.tier1_val or beautiful.bat_tier1_val or 25
-  args.tier2_val = args.tier2_val or beautiful.bat_tier2_val or 50
-  args.tier3_val = args.tier3_val or beautiful.bat_tier3_val or 75
-  args.tier4_val = args.tier4_val or beautiful.bat_tier4_val or 100
-
-  args.tier1_clr_discharging = args.tier1_clr_discharging
-    or beautiful.bat_tier1_clr_discharging
-    or args.fg
-  args.tier2_clr_discharging = args.tier2_clr_discharging
-    or beautiful.bat_tier2_clr_discharging
-    or args.fg
-  args.tier3_clr_discharging = args.tier3_clr_discharging
-    or beautiful.bat_tier3_clr_discharging
-    or args.fg
-  args.tier4_clr_discharging = args.tier4_clr_discharging
-    or beautiful.bat_tier4_clr_discharging
-    or args.fg
-
-  args.icon_discharging = args.icon_discharging
-    or beautiful.bat_icon_discharging
-    or " "
-  args.icon_discharging_0 = args.icon_discharging_0
-    or beautiful.bat_icon_discharging_0
-    or args.icon_discharging
-  args.icon_discharging_1 = args.icon_discharging_1
-    or beautiful.bat_icon_discharging_1
-    or args.icon_discharging
-  args.icon_discharging_2 = args.icon_discharging_2
-    or beautiful.bat_icon_discharging_2
-    or args.icon_discharging
-  args.icon_discharging_3 = args.icon_discharging_3
-    or beautiful.bat_icon_discharging_3
-    or args.icon_discharging
-  args.icon_discharging_4 = args.icon_discharging_4
-    or beautiful.bat_icon_discharging_4
-    or args.icon_discharging
-
-  args.tier1_clr_charging = args.tier1_clr_charging
-    or beautiful.bat_tier1_clr_charging
-    or args.fg
-  args.tier2_clr_charging = args.tier2_clr_charging
-    or beautiful.bat_tier2_clr_charging
-    or args.fg
-  args.tier3_clr_charging = args.tier3_clr_charging
-    or beautiful.bat_tier3_clr_charging
-    or args.fg
-  args.tier4_clr_charging = args.tier4_clr_charging
-    or beautiful.bat_tier4_clr_charging
-    or args.fg
-
-  args.icon_charging = args.icon_charging
-    or beautiful.bat_icon_charging
-    or " "
-  args.icon_charging_0 = args.icon_charging_0
-    or beautiful.bat_icon_charging_0
-    or args.icon_charging
-  args.icon_charging_1 = args.icon_charging_1
-    or beautiful.bat_icon_charging_1
-    or args.icon_charging
-  args.icon_charging_2 = args.icon_charging_2
-    or beautiful.bat_icon_charging_2
-    or args.icon_charging
-  args.icon_charging_3 = args.icon_charging_3
-    or beautiful.bat_icon_charging_3
-    or args.icon_charging
-  args.icon_charging_4 = args.icon_charging_4
-    or beautiful.bat_icon_charging_4
-    or args.icon_charging
-
-  args.command_status = args.command_status
-    or beautiful.bat_command_status
-    or default_commands.status
-  args.command_capacity = args.command_capacity
-    or beautiful.bat_command_capacity
-    or default_commands.capacity
-
-  local function compute_tier_clr(value)
-    local key
-    if status == "Discharging" then
-      key = "_clr_discharging"
-    else
-      key = "_clr_charging"
+    if status == "Discharging" and value <= options.alert_value then
+      naughty.notify({
+        icon = options.alert_icon,
+        icon_size = beautiful.apply_dpi(34),
+        title = "Battery Alert !",
+        text = "Warning, Battery below " .. options.alert_value .. "% !",
+        urgency = "critical",
+      })
     end
-    if value <= args.tier1_val then
-      return args["tier1" .. key]
-    elseif value <= args.tier2_val then
-      return args["tier2" .. key]
-    elseif value <= args.tier3_val then
-      return args["tier3" .. key]
-    else
-      return args["tier4" .. key]
-    end
-  end
 
-  local function compute_tier_icon(value, status)
-    local key = ""
-    if status == "Discharging" then
-      key = "icon_discharging_"
-    else
-      key = "icon_charging_"
-    end
-    if value <= args.tier1_val then
-      return args[key .. "1"]
-    elseif value <= args.tier2_val then
-      return args[key .. "2"]
-    elseif value <= args.tier3_val then
-      return args[key .. "3"]
-    else
-      return args[key .. "4"]
-    end
-  end
-
-  local function get_battery_value()
-    return {
-      remain = tonumber(os.capture(args.command_capacity)),
-      status = os.capture(args.command_status),
-    }
+    widget.fg = options["clr_" .. clr_idx]
+    widget:get_children_by_id("bat_icon")[1].text =
+      options["icon_" .. string.lower(status) .. "_" .. clr_idx]
+    widget:get_children_by_id("bat_value")[1].text =
+      string.format(" %03.1f%%", value)
   end
 
   bat = wibox.widget({
@@ -180,62 +84,23 @@ local function factory(args)
       {
         {
           id = "bat_icon",
-          text = compute_tier_icon(50),
-          font = args.font,
           widget = wibox.widget.textbox,
         },
         {
           id = "bat_value",
           widget = wibox.widget.textbox,
-          text = string.format(" %03.1f%% ", 50.0),
-          ellipsize = "end",
-          wrap = "word_char",
-          valign = "center",
-          align = "center",
-          font = args.bat_font,
-          forced_height = args.height,
-          visible = true,
         },
-        layout = wibox.layout.align.horizontal,
+        layout = wibox.layout.fixed.horizontal,
       },
-      widget = wibox.container.margin(_, 15, 15, 0, 0),
+      widget = wibox.container.margin(nil, 15, 15, 0, 0),
     },
-    fg = compute_tier_clr(50),
-    bg = args.bg,
-    shape = args.shape,
+    font = beautiful.bat_font or beautiful.font,
+    bg = beautiful.bat_bg or beautiful.widget_default_bg,
+    shape = beautiful.bat_shape or beautiful.widget_default_shape_right,
     widget = wibox.container.background,
-    set_bat_remain = function(self, remain, status)
-      local clr = compute_tier_clr(remain)
-      local icon = compute_tier_icon(remain, status)
-      if status == "Discharging" and remain <= args.alert_value then
-        naughty.notify({
-          preset = naughty.config.presets.critical,
-          timeout = 2,
-          title = "Battery Alert !",
-          text = "<span foreground= '#FFFFFF'>Warning, Battery below "
-            .. args.alert_value
-            .. "% !</span>",
-        })
-      end
-      -- Update widget values
-      self.fg = clr
-      self:get_children_by_id("bat_icon")[1].text = icon
-      self:get_children_by_id("bat_value")[1].text =
-        string.format(" %03.1f%% ", remain)
-    end,
   })
 
-  local update_widget_remain = function(widget, _, _, _, _)
-    local bat_value = get_battery_value()
-    widget:set_bat_remain(bat_value.remain, bat_value.status)
-  end
-
-  awful.widget.watch(
-    default_commands.capacity,
-    args.timeout,
-    update_widget_remain,
-    bat
-  )
+  awful.widget.watch("echo &>/dev/null", options.timeout, update_widget, bat)
 
   return bat
 end
