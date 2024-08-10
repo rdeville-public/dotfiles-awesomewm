2ca15ab80bfb67758a8218c9a56af2a7976c8476
Romain Deville <code@romaindeville.fr>
2024-08-10 13:59:54 +0200

    ⚡️(widget/cc/sliders): Improver overall control center sliders

--- widgets/control_center/sliders/microphone.lua
+++ widgets/control_center/sliders/microphone.lua
@@ -36,8 +36,8 @@ local slider_widget = wibox.widget({
 
       bar_shape = gears.shape.rounded_rect,
       bar_height = dpi(3),
-      bar_color = beautiful.cc_slider_volume_bar_inactive_bg
-        or beautiful.cc_button_inactive_bg
+      bar_color = beautiful.cc_brightness_bar_inactive_bg
+        or beautiful.cc_button_inactive
         or beautiful.cc_button_inactive_default_bg,
 
       handle_shape = gears.shape.circle,
@@ -64,44 +64,50 @@ local slider_widget = wibox.widget({
   layout = wibox.layout.align.horizontal,
 })
 
+local function is_muted()
+  local muted = os.capture(beautiful.cc_microphone_command_check_mute or [=[
+    pulsemixer --list-sources | grep 'Default' | sed 's/.*Mute: \([01]\).*/\1/'
+  ]=])
+  if muted == "0" then
+    return false
+  end
+  return true
+end
+
 local function set_status()
-  local vol = os.capture(beautiful.cc_slider_volume_command_get_volume or [=[
-    default_sink=$(pulsemixer --list-sinks \
+  local vol = os.capture(beautiful.cc_microphone_command_get_volume or [=[
+    default_source=$(pulsemixer --list-sources \
       | grep 'Default' \
       | sed 's/.*ID: \(.*\), Name:.*/\1/g')
-    pulsemixer --id ${default_sink} --get-volume | awk '{print $1}'
-  ]=])
-  local muted = os.capture(beautiful.cc_slider_volume_command_check_mute or [=[
-    pulsemixer --list-sinks | grep 'Default' | sed 's/.*Mute: \([01]\).*/\1/'
+    pulsemixer --id ${default_source} --get-volume | awk '{print $1}'
   ]=])
   local icon = slider_widget:get_children_by_id("icon")[1]
   local icon_bg = slider_widget:get_children_by_id("icon_bg")[1]
   local slider = slider_widget:get_children_by_id("slider")[1]
   local value = slider_widget:get_children_by_id("value")[1]
 
-  if muted:match("1") then
-    icon.image = beautiful.cc_slider_volume_muted_icon_path
-      or script_path() .. "../icons/volume-muted.svg"
-
-    icon_bg.bg = beautiful.cc_slider_volume_inactive_color
+  if is_muted() then
+    icon.image = beautiful.cc_microphone_muted_icon_path
+      or script_path() .. "../icons/microphone-muted.svg"
+    icon_bg.bg = beautiful.cc_microphone_inactive_bg
       or beautiful.cc_button_inactive_bg
       or beautiful.cc_button_inactive_default_bg
-    slider.bar_active_color = beautiful.cc_slider_volume_inactive_color
+    slider.bar_active_color = beautiful.cc_microphone_active_bg
+      or beautiful.cc_button_active_bg
+      or beautiful.cc_button_active_default
+    slider.handle_color = beautiful.cc_microphone_inactive_bg
       or beautiful.cc_button_inactive_bg
       or beautiful.cc_button_inactive_default_bg
-    slider.handle_color = beautiful.cc_slider_volume_inactive_color
-      or beautiful.cc_button_inactive_bg
-      or beautiful.cc_button_inactive_default_bg
-  elseif muted:match("0") then
-    icon.image = beautiful.cc_slider_volume_normal_icon_path
-      or script_path() .. "../icons/volume-normal.svg"
-    icon_bg.bg = beautiful.cc_slider_volume_active_color
+  else
+    icon.image = beautiful.cc_microphone_normal_icon_path
+      or script_path() .. "../icons/microphone-normal.svg"
+    icon_bg.bg = beautiful.cc_microphone_active_bg
       or beautiful.cc_button_active_bg
       or beautiful.cc_button_active_default_bg
-    slider.bar_active_color = beautiful.cc_slider_volume_active_color
-      or beautiful.cc_button_active_bg
+    slider.bar_active_color = beautiful.cc_microphone_active_bg
+      or beautiful.cc_button_active
       or beautiful.cc_button_active_default_bg
-    slider.handle_color = beautiful.cc_slider_volume_active_color
+    slider.handle_color = beautiful.cc_microphone_active_bg
       or beautiful.cc_button_active_bg
       or beautiful.cc_button_active_default_bg
   end
@@ -111,18 +117,20 @@ local function set_status()
 end
 
 local toggle_mute = function()
-  local muted = os.capture(beautiful.cc_slider_volume_command_check_mute or [=[
-    pulsemixer --list-sinks | grep 'Default' | sed 's/.*Mute: \([01]\).*/\1/'
+  if is_muted() then
+    awful.spawn.with_shell(beautiful.cc_microphone_command_unmute or [=[
+    default_source=$(pulsemixer --list-sources \
+      | grep 'Default' \
+      | sed 's/.*ID: \(.*\), Name:.*/\1/g')
+    pulsemixer --id ${default_source} --unmute
   ]=])
-
-  if muted == "0" then
-    awful.spawn.with_shell(
-      beautiful.cc_slider_volume_command_mute or "pulsemixer --mute"
-    )
   else
-    awful.spawn.with_shell(
-      beautiful.cc_slider_volume_command_unmute or "pulsemixer --unmute"
-    )
+    awful.spawn.with_shell(beautiful.cc_microphone_command_mute or [=[
+    default_source=$(pulsemixer --list-sources \
+      | grep 'Default' \
+      | sed 's/.*ID: \(.*\), Name:.*/\1/g')
+    pulsemixer --id ${default_source} --mute
+  ]=])
   end
   set_status()
 end
@@ -131,10 +139,15 @@ end
 slider_widget
   :get_children_by_id("slider")[1]
   :connect_signal("property::value", function(_, value)
-    awful.spawn.with_shell("pulsemixer --set-volume " .. value)
+    awful.spawn.with_shell(beautiful.cc_microphone_command_set or ([[
+    default_source=$(pulsemixer --list-sources \
+      | grep 'Default' \
+      | sed 's/.*ID: \(.*\), Name:.*/\1/g')
+    pulsemixer --id ${default_source} --set-volume ]] .. value))
     slider_widget:get_children_by_id("value")[1].text = value .. "%"
   end)
 
+-- When pressing icon
 slider_widget
   :get_children_by_id("icon")[1]
   :connect_signal("button::press", function()
