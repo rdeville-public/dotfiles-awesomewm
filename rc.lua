4504ac9f32e2c0cbb1e088d70582f4e683f7a577
Romain Deville <code@romaindeville.fr>
2024-08-13 00:50:17 +0200

    🔧 Update rc.lua

--- rc.lua
+++ rc.lua
@@ -2,24 +2,23 @@
 -- Awesome libraries
 -- Widget library
 require("awful.autofocus")
-local awful = require("awful")
 require("gears")
 require("naughty")
 require("awful.rules")
--- Theme library
-local beautiful = require("beautiful")
 -- Personal tools, mainly for run_once method
 require("utils.utility")
 
+local awful = require("awful")
 -- Set awful sh application
 awful.util.shell = "zsh"
-awesome.set_preferred_icon_size(24)
-beautiful.init(require("theme"))
-
 -- Set taglist, layous and tasklist
 awful.util.tagnames = require("config.tags").tags
 awful.layout.layouts = require("config.layouts")
 awful.util.tasklist_buttons = require("config.buttons.tasklist")
+
+-- Theme library
+local beautiful = require("beautiful")
+beautiful.init(require("theme"))
 
 -- Virtual Desktop
 -- ------------------------------------------------------------------------
@@ -31,8 +30,7 @@ end)
 
 -- Set key bindings
 require("config.keys.client")
-local globalkeys = require("config.keys.global")
-root.keys(globalkeys)
+root.keys(require("config.keys.global"))
 
 -- Set Signals
 require("config.signals.naughty")
@@ -45,7 +43,7 @@ require("config.signals.ruled")
 require("config.ruled.client")
 
 -- STARTUP RUN ONCE
--- ========================================================================
+-- ------------------------------------------------------------------------
 run_once({
   "picom",
   "keynav",
